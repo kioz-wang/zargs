60d488039666babfeaf6fd86f8024c84785533df
kioz.wang <never.had@outlook.com>
2025-03-05 02:10:58 +0800

    [examples][build] try to add examples build, but fail yet

--- build.zig
+++ build.zig
@@ -18,15 +18,51 @@ pub fn build(b: *std.Build) void {
         .strip = true,
     });
     exe.root_module.addImport("zargs", mod);
-    b.installArtifact(exe);
+    const exe_install = b.addInstallArtifact(exe, .{});
+    exe_install.step.dependOn(&exe.step);
     const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
+    run_cmd.step.dependOn(&exe_install.step);
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
 
-    const run_step = b.step("run", "Run the example");
+    const run_step = b.step("run", "Run main example");
     run_step.dependOn(&run_cmd.step);
+
+    const examples_step = b.step("examples", "Build examples");
+
+    if (std.fs.cwd().openDir("example", .{ .iterate = true })) |d| {
+        var it = d.iterate();
+        const example_prefix = "ex-";
+        const example_suffix = ".zig";
+        while (it.next() catch null) |e| {
+            if (e.kind != .file) {
+                continue;
+            }
+            if (!std.mem.startsWith(u8, e.name, example_prefix)) {
+                continue;
+            }
+            if (!std.mem.endsWith(u8, e.name, example_suffix)) {
+                continue;
+            }
+            var exe_name = e.name[example_prefix.len..];
+            exe_name = exe_name[0..(exe_name.len - example_suffix.len)];
+            const example = b.addExecutable(.{
+                .name = exe_name,
+                .root_source_file = b.path("example").path(b, e.name),
+                .target = target,
+                .optimize = optimize,
+                .strip = true,
+            });
+            example.root_module.addImport("zargs", mod);
+            const example_install = b.addInstallArtifact(example, .{});
+            example_install.step.dependOn(&example.step);
+
+            examples_step.dependOn(&example_install.step);
+        }
+    } else |err| {
+        std.log.err("NotFound examples {any}", .{err});
+    }
 
     const test_step = b.step("test", "Run unit tests");
     const run_ut = b.addRunArtifact(b.addTest(.{
