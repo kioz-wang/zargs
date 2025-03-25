3b26d897eebec558932ffcb8a8fe41953732fbd8
kioz.wang <never.had@outlook.com>
2025-03-26 02:59:52 +0800

    Feature: [helper]implements `Collection.Range(T)`; improved `Compare.equal`
    other code optimize:
    - Improved compileError log
    - rename `B` to `T`
    - use `std.meta.hasMethod` instead of `@hasDecl`

--- src/helper.zig
+++ src/helper.zig
@@ -41,6 +41,103 @@ pub const Collection = struct {
         try testing.expect(set.contain("a"));
         try testing.expect(!set.add("a"));
     }
+
+    pub fn Range(T: type) type {
+        const _compare = Compare.compare;
+        return struct {
+            const Self = @This();
+            left: ?T,
+            right: ?T,
+            pub fn init(l: ?T, r: ?T) Self {
+                return .{ .left = l, .right = r };
+            }
+            const empty = Self.init(std.mem.zeroes(T), std.mem.zeroes(T));
+            const universal = Self.init(null, null);
+            pub fn is_empty(self: Self) bool {
+                return self.left != null and self.right != null and _compare(self.left.?, self.right.?) == .Equal;
+            }
+            pub fn is_universal(self: Self) bool {
+                return self.left == null and self.right == null;
+            }
+            pub fn format(self: Self, comptime _: []const u8, _: Alias.FormatOptions, writer: anytype) @TypeOf(writer).Error!void {
+                const s_empty = "∅";
+                const s_universal = "U";
+                const s_infinity = "∞";
+                if (self.is_empty()) {
+                    try writer.writeAll(s_empty);
+                    return;
+                }
+                if (self.is_universal()) {
+                    try writer.writeAll(s_universal);
+                    return;
+                }
+                if (self.left) |left| {
+                    try writer.writeAll(print("[{s}", .{formatAny(left)}));
+                } else {
+                    try writer.writeAll(print("(-{s}", .{s_infinity}));
+                }
+                try writer.writeAll(print(",{s})", .{if (self.right) |right| formatAny(right) else s_infinity}));
+            }
+            pub fn contain(self: Self, v: T) bool {
+                if (self.left) |left| {
+                    if (_compare(left, v) == .Greater) return false;
+                }
+                if (self.right) |right| {
+                    if (_compare(v, right) != .Less) return false;
+                }
+                return true;
+            }
+            pub fn compare(self: Self, v: Self) Compare.Order {
+                return if (self.left) |left|
+                    if (v.left) |v_left| _compare(left, v_left) else .Greater
+                else if (v.left) |_| .Less else .Equal;
+            }
+        };
+    }
+
+    test "Range format" {
+        try testing.expectEqualStrings("U", print("{}", .{Range(u32).universal}));
+        try testing.expectEqualStrings("∅", print("{}", .{Range(u32).empty}));
+        try testing.expectEqualStrings("(-∞,1)", print("{}", .{comptime Range(i32).init(null, 1)}));
+        try testing.expectEqualStrings("[-1,1)", print("{}", .{comptime Range(i32).init(-1, 1)}));
+        try testing.expectEqualStrings("[-1,∞)", print("{}", .{comptime Range(i32).init(-1, null)}));
+    }
+
+    test "Range contain" {
+        try testing.expect(Range(u32).universal.contain(std.mem.zeroes(u32)));
+        try testing.expect(Range(u32).universal.contain(1));
+        try testing.expect(Range(u32).universal.contain(9999));
+        try testing.expect(!Range(u32).empty.contain(std.mem.zeroes(u32)));
+        try testing.expect(!Range(u32).empty.contain(1));
+        try testing.expect(!Range(u32).empty.contain(9999));
+        {
+            const range = Range(i32).init(null, 5);
+            try testing.expect(range.contain(-1000));
+            try testing.expect(range.contain(0));
+            try testing.expect(!range.contain(5));
+        }
+        {
+            const range = Range(i32).init(-5, 5);
+            try testing.expect(!range.contain(-1000));
+            try testing.expect(range.contain(-5));
+            try testing.expect(range.contain(0));
+            try testing.expect(!range.contain(5));
+        }
+        {
+            const range = Range(i32).init(-5, null);
+            try testing.expect(!range.contain(-1000));
+            try testing.expect(range.contain(-5));
+            try testing.expect(range.contain(0));
+            try testing.expect(range.contain(5));
+        }
+    }
+
+    test "Range compare" {
+        const compare = Compare.compare;
+        try testing.expectEqual(compare(Range(i32).empty, Range(i32).universal), .Greater);
+        try testing.expectEqual(compare(Range(i32).empty, Range(i32).init(0, null)), .Equal);
+        try testing.expectEqual(compare(Range(i32).empty, Range(i32).init(1, null)), .Less);
+    }
 };
 
 pub fn upper(comptime str: []const u8) [str.len]u8 {
@@ -74,17 +171,17 @@ pub const Compare = struct {
     ///
     /// for enum and struct, if find `pub fn compare(self: T, v: T) Order`, use it
     pub fn compare(a: anytype, b: @TypeOf(a)) Order {
-        const B = @TypeOf(a);
-        if (B == comptime_int or B == comptime_float) return if (a > b) .Greater else if (a == b) .Equal else .Less;
-        return switch (@typeInfo(B)) {
+        const T = @TypeOf(a);
+        if (T == comptime_int or T == comptime_float) return if (a > b) .Greater else if (a == b) .Equal else .Less;
+        return switch (@typeInfo(T)) {
             .int, .float => if (a > b) .Greater else if (a == b) .Equal else .Less,
-            .@"enum" => if (@hasDecl(B, "compare")) a.compare(b) else blk: {
+            .@"enum" => if (std.meta.hasMethod(T, "compare")) a.compare(b) else blk: {
                 const _a = @intFromEnum(a);
                 const _b = @intFromEnum(b);
                 break :blk if (_a > _b) .Greater else if (_a == _b) .Equal else .Less;
             },
-            .@"struct" => if (@hasDecl(B, "compare")) a.compare(b) else @compileError(print("require comparer for {s}", .{@typeName(B)})),
-            else => @compileError(print("unable compare in {s}", .{@typeName(B)})),
+            .@"struct" => if (std.meta.hasMethod(T, "compare")) a.compare(b) else @compileError(print("No compare method implemented for {s}", .{@typeName(T)})),
+            else => @compileError(print("No compare method implemented for {s}", .{@typeName(T)})),
         };
     }
 
@@ -94,34 +191,37 @@ pub const Compare = struct {
     ///
     /// for packed struct, just check equal directly
     pub fn equal(a: anytype, b: @TypeOf(a)) bool {
-        const B = @TypeOf(a);
-        if (B == String) return std.mem.eql(u8, a, b);
-        if (B == comptime_int or B == comptime_float) return a == b;
-        return switch (@typeInfo(B)) {
-            .int, .float, .bool, .@"enum" => a == b,
-            .@"struct" => |info| if (@hasDecl(B, "equal")) a.equal(b) else if (info.layout == .@"packed") a == b else @compileError(print("require equaler for {s}", .{@typeName(B)})),
-            else => @compileError(print("unable check equal in {s}", .{@typeName(B)})),
+        const T = @TypeOf(a);
+        if (T == String) return std.mem.eql(u8, a, b);
+        if (T == comptime_int or T == comptime_float) return a == b;
+        return switch (@typeInfo(T)) {
+            .int, .float, .bool => a == b,
+            .@"enum" => if (std.meta.hasMethod(T, "equal")) a.equal(b) else if (std.meta.hasMethod(T, "compare")) a.compare(b) == Order.Equal else a == b,
+            .@"struct" => |info| if (std.meta.hasMethod(T, "equal")) a.equal(b) else if (std.meta.hasMethod(T, "compare")) a.compare(b) == Order.Equal else if (info.layout == .@"packed") a == b else @compileError(print("No equal method implemented for {s}", .{@typeName(T)})),
+            else => @compileError(print("No equal method implemented for {s}", .{@typeName(T)})),
         };
     }
 
+    // TODO implement sort algo using compare and equal
+
     test compare {
-        try testing.expectEqual(compare(1, 2), Order.Less);
-        try testing.expectEqual(compare(2, 2), Order.Equal);
-        try testing.expectEqual(compare(3, 2), Order.Greater);
-        try testing.expectEqual(compare(@as(u32, 1), 2), Order.Less);
-        try testing.expectEqual(compare(@as(u32, 2), 2), Order.Equal);
-        try testing.expectEqual(compare(@as(u32, 3), 2), Order.Greater);
-        try testing.expectEqual(compare(1.0, 2.0), Order.Less);
-        try testing.expectEqual(compare(2.0, 2.0), Order.Equal);
-        try testing.expectEqual(compare(3.0, 2.0), Order.Greater);
-        try testing.expectEqual(compare(@as(f32, 1.0), 2.0), Order.Less);
-        try testing.expectEqual(compare(@as(f32, 2.0), 2.0), Order.Equal);
-        try testing.expectEqual(compare(@as(f32, 3.0), 2.0), Order.Greater);
+        try testing.expectEqual(compare(1, 2), .Less);
+        try testing.expectEqual(compare(2, 2), .Equal);
+        try testing.expectEqual(compare(3, 2), .Greater);
+        try testing.expectEqual(compare(@as(u32, 1), 2), .Less);
+        try testing.expectEqual(compare(@as(u32, 2), 2), .Equal);
+        try testing.expectEqual(compare(@as(u32, 3), 2), .Greater);
+        try testing.expectEqual(compare(1.0, 2.0), .Less);
+        try testing.expectEqual(compare(2.0, 2.0), .Equal);
+        try testing.expectEqual(compare(3.0, 2.0), .Greater);
+        try testing.expectEqual(compare(@as(f32, 1.0), 2.0), .Less);
+        try testing.expectEqual(compare(@as(f32, 2.0), 2.0), .Equal);
+        try testing.expectEqual(compare(@as(f32, 3.0), 2.0), .Greater);
         {
             const Color = enum { Red, Green, Blue };
-            try testing.expectEqual(compare(Color.Red, Color.Green), Order.Less);
-            try testing.expectEqual(compare(Color.Green, Color.Green), Order.Equal);
-            try testing.expectEqual(compare(Color.Blue, Color.Green), Order.Greater);
+            try testing.expectEqual(compare(Color.Red, Color.Green), .Less);
+            try testing.expectEqual(compare(Color.Green, Color.Green), .Equal);
+            try testing.expectEqual(compare(Color.Blue, Color.Green), .Greater);
         }
         {
             const Color = enum {
@@ -134,23 +234,22 @@ pub const Compare = struct {
                     return if (_a > _b) .Greater else if (_a == _b) .Equal else .Less;
                 }
             };
-            try testing.expectEqual(compare(Color.Red, Color.Green), Order.Less);
-            try testing.expectEqual(compare(Color.Green, Color.Green), Order.Equal);
-            try testing.expectEqual(compare(Color.Blue, Color.Green), Order.Greater);
+            try testing.expectEqual(compare(Color.Red, Color.Green), .Less);
+            try testing.expectEqual(compare(Color.Green, Color.Green), .Equal);
+            try testing.expectEqual(compare(Color.Blue, Color.Green), .Greater);
         }
         {
             const Person = struct {
                 age: u32,
                 name: String = undefined,
+                const _compare = Compare.compare;
                 pub fn compare(self: @This(), v: @This()) Order {
-                    const _a = self.age;
-                    const _b = v.age;
-                    return if (_a > _b) .Greater else if (_a == _b) .Equal else .Less;
+                    return _compare(self.age, v.age);
                 }
             };
-            try testing.expectEqual(compare(Person{ .age = 1 }, Person{ .age = 2 }), Order.Less);
-            try testing.expectEqual(compare(Person{ .age = 2 }, Person{ .age = 2 }), Order.Equal);
-            try testing.expectEqual(compare(Person{ .age = 3 }, Person{ .age = 2 }), Order.Greater);
+            try testing.expectEqual(compare(Person{ .age = 1 }, Person{ .age = 2 }), .Less);
+            try testing.expectEqual(compare(Person{ .age = 2 }, Person{ .age = 2 }), .Equal);
+            try testing.expectEqual(compare(Person{ .age = 3 }, Person{ .age = 2 }), .Greater);
         }
     }
 
@@ -182,6 +281,18 @@ pub const Compare = struct {
                 name: String = undefined,
                 pub fn equal(self: @This(), v: @This()) bool {
                     return self.age == v.age;
+                }
+            };
+            try testing.expect(equal(Person{ .age = 2 }, Person{ .age = 2 }));
+            try testing.expect(!equal(Person{ .age = 1 }, Person{ .age = 2 }));
+        }
+        {
+            const Person = struct {
+                age: u32,
+                name: String = undefined,
+                const _compare = Compare.compare;
+                pub fn compare(self: @This(), v: @This()) Order {
+                    return _compare(self.age, v.age);
                 }
             };
             try testing.expect(equal(Person{ .age = 2 }, Person{ .age = 2 }));
@@ -255,14 +366,14 @@ pub const Type = struct {
     }
 };
 
-fn formatBase(v: anytype) []const u8 {
+pub fn formatAny(v: anytype) []const u8 {
     const T = @TypeOf(v);
     if (T == String) return print("{s}", .{v});
     return switch (@typeInfo(T)) {
         .int, .bool, .@"struct" => print("{any}", .{v}),
         .float => print("{:.3}", .{v}),
         .@"enum" => print("{s}", .{@tagName(v)}),
-        else => unreachable,
+        else => @compileError(print("No formatAny implemented for {s}", .{@typeName(T)})),
     };
 }
 
@@ -272,11 +383,11 @@ pub fn NiceFormatter(T: type) type {
         v: T,
         pub fn format(self: Self, comptime _: []const u8, _: Alias.FormatOptions, writer: anytype) @TypeOf(writer).Error!void {
             if (Type.Base(T) == T) {
-                try writer.writeAll(formatBase(self.v));
+                try writer.writeAll(formatAny(self.v));
                 return;
             }
             if (Type.isOptional(T)) {
-                try writer.writeAll(if (self.v) |v_| formatBase(v_) else "null");
+                try writer.writeAll(if (self.v) |v_| formatAny(v_) else "null");
                 return;
             }
             if (Type.isMultiple(T)) {
@@ -285,7 +396,7 @@ pub fn NiceFormatter(T: type) type {
                     if (i != 0) {
                         try writer.writeAll(", ");
                     }
-                    try writer.writeAll(formatBase(v_));
+                    try writer.writeAll(formatAny(v_));
                 }
                 try writer.writeAll("}");
                 return;
--- src/parser.zig
+++ src/parser.zig
@@ -10,36 +10,34 @@ const String = helper.Alias.String;
 /// for enum and struct, if find `pub fn parse(s: String, a: ?Allocator) ?T`, use it
 ///
 /// for String, allocate if Allocator is given
-pub fn parseAny(B: type, s: String, a: ?Allocator) ?B {
-    if (B == String) {
+pub fn parseAny(T: type, s: String, a: ?Allocator) ?T {
+    if (T == String) {
         return if (a) |allocator| blk: {
             const allocS = allocator.alloc(u8, s.len) catch return null;
             @memcpy(allocS, s);
             break :blk allocS;
         } else s;
     }
-    return switch (@typeInfo(B)) {
-        .int => std.fmt.parseInt(B, s, 0) catch null,
-        .float => std.fmt.parseFloat(B, s) catch null,
+    return switch (@typeInfo(T)) {
+        .int => std.fmt.parseInt(T, s, 0) catch null,
+        .float => std.fmt.parseFloat(T, s) catch null,
         .bool => helper.Parser.boolean(s),
-        .@"enum" => if (@hasDecl(B, "parse")) B.parse(s, a) else std.meta.stringToEnum(B, s),
-        .@"struct" => if (@hasDecl(B, "parse")) B.parse(s, a) else @compileError(print("require parser for {s}", .{@typeName(B)})),
-        else => {
-            @compileError(print("unable parse to {s}", .{@typeName(B)}));
-        },
+        .@"enum" => if (std.meta.hasMethod(T, "parse")) T.parse(s, a) else std.meta.stringToEnum(T, s),
+        .@"struct" => if (std.meta.hasMethod(T, "parse")) T.parse(s, a) else @compileError(print("No parse method implemented for {s}", .{@typeName(T)})),
+        else => @compileError(print("No parse method implemented for {s}", .{@typeName(T)})),
     };
 }
 
 /// destroy any base T value
 ///
 /// for struct, if find `pub fn destroy(self: T, a: Allocator) void`, use it
-pub fn destroyAny(B: type, v: B, a: Allocator) void {
-    if (B == String) {
+pub fn destroyAny(T: type, v: T, a: Allocator) void {
+    if (T == String) {
         a.free(v);
         return;
     }
-    switch (@typeInfo(B)) {
-        .@"struct" => if (@hasDecl(B, "destroy")) v.destroy(a),
+    switch (@typeInfo(T)) {
+        .@"struct" => if (std.meta.hasMethod(T, "destroy")) v.destroy(a),
         else => {},
     }
 }
